d7d320c499899f2911548ec610db55bc0a12f029
Rafael Aranzate <53230184+aranzate@users.noreply.github.com>
2023-09-17 21:41:15 -0300

    Delete api/src/main/java/com/webshop/api directory

--- api/src/main/java/com/webshop/api/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.webshop.api.repository;
-
-import com.webshop.api.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
